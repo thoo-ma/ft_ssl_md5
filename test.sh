905ff93931c9a21de5804eaacc382ea85ca2856b
thoo-ma <trobin@student.42.fr>
2025-02-01 13:21:03 +0100

    factorize tester

--- test.sh
+++ test.sh
@@ -1,36 +1,25 @@
 #!/bin/bash
 
-# ___________________ MD5 ___________________
+run_test_files() {
+  local algo=$1
+  shift
+  for file in "$@"; do
+    openssl $algo $file
+    ./ft_ssl $algo $file
+    echo ''
+  done
+}
 
-openssl  md5 readme
-./ft_ssl md5 readme
-echo ''
+run_test_stdin() {
+  local algo=$1
+  local input=$2
+  echo "$input" | openssl $algo
+  echo "$input" | ./ft_ssl $algo
+  echo ''
+}
 
-openssl  md5 Makefile
-./ft_ssl md5 Makefile
-echo ''
+run_test_files md5 readme Makefile
+run_test_stdin md5 '42 is nice'
 
-openssl  md5 readme Makefile
-./ft_ssl md5 readme Makefile
-echo ''
-
-echo '42 is nice' | openssl  md5
-echo '42 is nice' | ./ft_ssl md5
-echo ''
-
-# ___________________ SHA256 ________________
-
-echo '42 is nice' | openssl  sha256
-echo '42 is nice' | ./ft_ssl sha256
-echo ''
-
-openssl  sha256 readme
-./ft_ssl sha256 readme
-echo ''
-
-openssl  sha256 Makefile
-./ft_ssl sha256 Makefile
-echo ''
-
-openssl  sha256 readme Makefile
-./ft_ssl sha256 readme Makefile
+run_test_files sha256 readme Makefile
+run_test_stdin sha256 '42 is nice'
