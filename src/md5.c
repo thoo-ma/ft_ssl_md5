790a203747612a63ae058bde7247b8f308dc44aa
thoo-ma <trobin@student.42.fr>
2025-04-02 19:47:35 +0200

    unroll md5 update loop

--- src/md5.c
+++ src/md5.c
@@ -7,10 +7,10 @@
 #include "utils.h"
 
 static void md5_init(uint32_t hash[4]) {
-    hash[0] = md5_context.h0;
-    hash[1] = md5_context.h1;
-    hash[2] = md5_context.h2;
-    hash[3] = md5_context.h3;
+    hash[0] = 0x67452301;
+    hash[1] = 0xEFCDAB89;
+    hash[2] = 0x98BADCFE;
+    hash[3] = 0x10325476;
 }
 
 static void md5_pad(uint8_t chunk[CHUNK_SIZE_TOTAL], size_t * chunk_size, size_t message_size) {
@@ -65,35 +65,89 @@ static void md5_update(uint8_t chunk[CHUNK_SIZE_TOTAL], size_t chunk_size, uint3
         uint32_t c = c0;
         uint32_t d = d0;
 
-        // loop over each 32-bit word
-        for (uint8_t j = 0; j < 64; j++) {
+        // Round 1
+        FF(a, b, c, d, w[0],  7,  0xd76aa478);
+        FF(d, a, b, c, w[1],  12, 0xe8c7b756);
+        FF(c, d, a, b, w[2],  17, 0x242070db);
+        FF(b, c, d, a, w[3],  22, 0xc1bdceee);
+        
+        FF(a, b, c, d, w[4],  7,  0xf57c0faf);
+        FF(d, a, b, c, w[5],  12, 0x4787c62a);
+        FF(c, d, a, b, w[6],  17, 0xa8304613);
+        FF(b, c, d, a, w[7],  22, 0xfd469501);
+        
+        FF(a, b, c, d, w[8],  7,  0x698098d8);
+        FF(d, a, b, c, w[9],  12, 0x8b44f7af);
+        FF(c, d, a, b, w[10], 17, 0xffff5bb1);
+        FF(b, c, d, a, w[11], 22, 0x895cd7be);
+        
+        FF(a, b, c, d, w[12], 7,  0x6b901122);
+        FF(d, a, b, c, w[13], 12, 0xfd987193);
+        FF(c, d, a, b, w[14], 17, 0xa679438e);
+        FF(b, c, d, a, w[15], 22, 0x49b40821);
 
-            uint32_t f, g;
+        // Round 2
+        GG(a, b, c, d, w[1],  5,  0xf61e2562);
+        GG(d, a, b, c, w[6],  9,  0xc040b340);
+        GG(c, d, a, b, w[11], 14, 0x265e5a51);
+        GG(b, c, d, a, w[0],  20, 0xe9b6c7aa);
+        
+        GG(a, b, c, d, w[5],  5,  0xd62f105d);
+        GG(d, a, b, c, w[10], 9,  0x02441453);
+        GG(c, d, a, b, w[15], 14, 0xd8a1e681);
+        GG(b, c, d, a, w[4],  20, 0xe7d3fbc8);
+        
+        GG(a, b, c, d, w[9],  5,  0x21e1cde6);
+        GG(d, a, b, c, w[14], 9,  0xc33707d6);
+        GG(c, d, a, b, w[3],  14, 0xf4d50d87);
+        GG(b, c, d, a, w[8],  20, 0x455a14ed);
+        
+        GG(a, b, c, d, w[13], 5,  0xa9e3e905);
+        GG(d, a, b, c, w[2],  9,  0xfcefa3f8);
+        GG(c, d, a, b, w[7],  14, 0x676f02d9);
+        GG(b, c, d, a, w[12], 20, 0x8d2a4c8a);
 
-            if (j < 16) {
-                // round 1
-                f = (b & c) | ((~b) & d);
-                g = j;
-            } else if (j < 32) {
-                // round 2
-                f = (d & b) | ((~d) & c);
-                g = (5 * j + 1) % 16;
-            } else if (j < 48) {
-                // round 3
-                f = b ^ c ^ d;
-                g = (3 * j + 5) % 16;
-            } else {
-                // round 4
-                f = c ^ (b | (~d));
-                g = (7 * j) % 16;
-            }
+        // Round 3
+        HH(a, b, c, d, w[5],  4,  0xfffa3942);
+        HH(d, a, b, c, w[8],  11, 0x8771f681);
+        HH(c, d, a, b, w[11], 16, 0x6d9d6122);
+        HH(b, c, d, a, w[14], 23, 0xfde5380c);
+        
+        HH(a, b, c, d, w[1],  4,  0xa4beea44);
+        HH(d, a, b, c, w[4],  11, 0x4bdecfa9);
+        HH(c, d, a, b, w[7],  16, 0xf6bb4b60);
+        HH(b, c, d, a, w[10], 23, 0xbebfbc70);
+        
+        HH(a, b, c, d, w[13], 4,  0x289b7ec6);
+        HH(d, a, b, c, w[0],  11, 0xeaa127fa);
+        HH(c, d, a, b, w[3],  16, 0xd4ef3085);
+        HH(b, c, d, a, w[6],  23, 0x04881d05);
+        
+        HH(a, b, c, d, w[9],  4,  0xd9d4d039);
+        HH(d, a, b, c, w[12], 11, 0xe6db99e5);
+        HH(c, d, a, b, w[15], 16, 0x1fa27cf8);
+        HH(b, c, d, a, w[2],  23, 0xc4ac5665);
 
-            uint32_t temp = d;
-            d = c;
-            c = b;
-            b = b + ROTATE_LEFT((a + f + md5_context.k[j] + w[g]), md5_context.r[j]);
-            a = temp;
-        }
+        // Round 4
+        II(a, b, c, d, w[0],  6,  0xf4292244);
+        II(d, a, b, c, w[7],  10, 0x432aff97);
+        II(c, d, a, b, w[14], 15, 0xab9423a7);
+        II(b, c, d, a, w[5],  21, 0xfc93a039);
+        
+        II(a, b, c, d, w[12], 6,  0x655b59c3);
+        II(d, a, b, c, w[3],  10, 0x8f0ccc92);
+        II(c, d, a, b, w[10], 15, 0xffeff47d);
+        II(b, c, d, a, w[1],  21, 0x85845dd1);
+        
+        II(a, b, c, d, w[8],  6,  0x6fa87e4f);
+        II(d, a, b, c, w[15], 10, 0xfe2ce6e0);
+        II(c, d, a, b, w[6],  15, 0xa3014314);
+        II(b, c, d, a, w[13], 21, 0x4e0811a1);
+        
+        II(a, b, c, d, w[4],  6,  0xf7537e82);
+        II(d, a, b, c, w[11], 10, 0xbd3af235);
+        II(c, d, a, b, w[2],  15, 0x2ad7d2bb);
+        II(b, c, d, a, w[9],  21, 0xeb86d391);
 
         a0 += a;
         b0 += b;
